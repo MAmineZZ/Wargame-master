2c64006c7c5a7c09e6cc4d6fc7b124b9ca2f94da
Mohamed Amine Zinddine <zinddinemohamedamine@gmail.com>
2022-04-20 14:34:36 +0200

    wargame

---
+++ src/modele/CleMap.java
@@ -0,0 +1,38 @@
+package modele;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+
+public class CleMap<K, V> extends HashMap<K, V> {
+
+    public CleMap() {
+        super();
+
+    }
+    public Object getFirstKey() {
+
+        Iterator<Map.Entry<K, V>> iterator = this.entrySet().iterator();
+
+        if (iterator.hasNext()) {
+            Map.Entry<K, V> mapEntry = iterator.next();
+            return mapEntry.getKey();
+        } else {
+            System.out.println("Plus de clef dans la map");
+            return null;
+        }
+
+    }
+    public int getSize() {
+        Iterator<Map.Entry<K, V>> iterator = this.entrySet().iterator();
+        int i = 0;
+        while (iterator.hasNext()) {
+            Map.Entry<K, V> mapEntry = iterator.next();
+            i++;
+        }
+        return i;
+
+    }
+
+}
